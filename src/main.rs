69b3577f7cd00724227bd25067f96184da4cbaaf
Daniel May <daniel.may@mailbox.org>
2020-08-15 20:50:58 +0200

    make watch options configurable

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-use anyhow::{Context, Result};
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::PathBuf;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
-const WATCH_PATH: &str = ".wormhole";
-const WATCH_DELAY: u64 = 3;
+use anyhow::{Context, Result};
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "Wormhole")]
+struct Opt {
+    #[structopt(short, long, required = true, parse(from_os_str))]
+    watch_dir: PathBuf,
+
+    #[structopt(short = "e", long, default_value = "2")]
+    watch_delay: u64
+}
 
 fn main() -> Result<()> {
+    let opt = Opt::from_args();
+
     let (tx, rx) = channel();
 
-    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(WATCH_DELAY))
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(opt.watch_delay))
         .context("Could not initialize file watcher for this platform.")?;
 
     watcher
-        .watch(WATCH_PATH, RecursiveMode::Recursive)
-        .context(format!("Could not watch directory '{}'.", WATCH_PATH))?;
+        .watch(&opt.watch_dir, RecursiveMode::Recursive)
+        .context(format!(
+            "Could not watch directory '{:#?}'.",
+            &opt.watch_dir
+        ))?;
 
     loop {
         match rx.recv() {
