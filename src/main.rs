816f4b2e56fd83058990485546f4d2347cbce982
Daniel May <daniel.may@mailbox.org>
2020-08-16 12:18:19 +0200

    setup config directory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use std::fs;
 use std::path::PathBuf;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
 use anyhow::{Context, Result};
+use app_dirs2::{get_app_root, AppDataType, AppInfo};
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use structopt::StructOpt;
+
+const RULES_FILE_NAME: &str = "rules.yaml";
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "Wormhole")]
@@ -13,11 +17,13 @@ struct Opt {
     watch_dir: PathBuf,
 
     #[structopt(short = "e", long, default_value = "2")]
-    watch_delay: u64
+    watch_delay: u64,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
+
+    setup_path()?;
 
     let (tx, rx) = channel();
 
@@ -37,6 +43,24 @@ fn main() -> Result<()> {
             Err(e) => eprintln!("{}", e),
         }
     }
+
+    Ok(())
+}
+
+fn setup_path() -> Result<()> {
+    let dir = get_app_root(
+        AppDataType::UserConfig,
+        &AppInfo {
+            name: "Wormhole",
+            author: "May",
+        },
+    )?;
+    fs::create_dir_all(&dir).context(format!(
+        "Could not create configuration directory '{:#?}'.",
+        &dir
+    ))?;
+
+    //let rules = dir.join(RULES_FILE_NAME);
 
     Ok(())
 }
